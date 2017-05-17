4126ac74ba8222f3d61cc302d68d28c870da79cf
qtotuan <quynh.totuan@gmail.com>
2017-05-17 16:37:48 -0400

    Add Character class

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -1,27 +1,36 @@
 require 'rest-client'
 require 'json'
 require 'pry'
+require_relative './character'
 
 def get_character_movies_from_api(character)
   #make the web request
   all_characters = RestClient.get('http://www.swapi.co/api/people/')
   character_hash = JSON.parse(all_characters)
 
-  i = 0
-  while i < character_hash.length do
-    if character_hash["results"][i]["name"].downcase == character
-      films = character_hash["results"][i]["films"]
-    end
-    i += 1
+  character_hash["results"].each do |character_hash|
+    Character.all << Character.new(character_hash["name"])
   end
 
-    films_array = []
-    films.each do |film_url|
-      result = RestClient.get(film_url)
-      films_array << JSON.parse(result)
-    end
+  found = Character.find_character_by_name(character)
+  found.add_film(character_hash)
+  binding.pry
 
-    films_array
+  # i = 0
+  # while i < character_hash.length do
+  #   if character_hash["results"][i]["name"].downcase == character
+  #     films = character_hash["results"][i]["films"]
+  #   end
+  #   i += 1
+  # end
+  #
+  #   films_array = []
+  #   films.each do |film_url|
+  #     result = RestClient.get(film_url)
+  #     films_array << JSON.parse(result)
+  #   end
+  #
+  #   films_array
 end
 
 def get_movie_from_api(movie)
---
+++ lib/character.rb
@@ -0,0 +1,49 @@
+class Character
+ attr_accessor :name, :films
+
+ @@all = []
+
+ def initialize(name)
+   @name = name
+   @films = []
+ end
+
+  def self.all
+    @@all
+  end
+
+  def add_film(character_hash)
+    i = 0
+    while i < character_hash.length do
+      if character_hash["results"][i]["name"].downcase == @name.downcase
+        films = character_hash["results"][i]["films"]
+      end
+      i += 1
+    end
+
+    films_array = []
+    films.each do |film_url|
+      result = RestClient.get(film_url)
+      films_array << JSON.parse(result)
+      end
+    end
+
+    films_array.each do |film_hash|
+      @films << film_hash["title"]
+    end
+
+  end
+
+ def self.find_character_by_name name
+    self.all.detect { |character| character.name.downcase == name }
+  end
+
+end
+
+# class Api_methods
+#
+# all_characters = RestClient.get('http://www.swapi.co/api/people/')
+# character_hash = JSON.parse(all_characters)
+# character_hash["results"].each {|character| @@all << Character.new(character['name'])}
+#
+# end
